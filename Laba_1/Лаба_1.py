3957f7728ecfb8235fe32df05ca0eb5ddcaa70d1
MazalTovGeksli <kaa1994g@mail.ru>
2017-10-20 01:53:36 +0700

    Улучшение
    После лекции про генераторы потребовалось улучшение

--- Laba_1/Лаба_1.py
+++ Laba_1/Лаба_1.py
@@ -1,20 +1,4 @@
 import re
 import requests
 
-words = []
-seen = []
-text = requests.get('http://www.mosigra.ru/').text.split(' ')
-for word in text:
-    m = re.findall('\w+@\w+\.\w+', word)
-    if m:
-        words += m
-for x in words:
-    if x not in seen:
-        seen.append(x)
-my_input = open("Адреса_Лаба_1.txt", "w", encoding='utf-8')
-my_input.write('\n'.join(seen))
-my_input.close()
-
-
-
-
+print({q for word in requests.get('http://www.mosigra.ru/').text.split(' ') for q in re.findall('\w+@\w+\.\w+', word)})
