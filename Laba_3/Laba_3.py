ef1dfd9dd435ecdbe7863793de89be9df24945e4
MazalTovGeksli <kaa1994g@mail.ru>
2017-10-26 00:49:15 +0700

    Патч 0.1
    После уточнения задания переделал

--- Laba_3/Laba_3.py
+++ Laba_3/Laba_3.py
@@ -2,17 +2,28 @@ import requests
 from bs4 import BeautifulSoup
 
 soup = BeautifulSoup(requests.get('https://www.washingtonpost.com/').text, "html.parser")
-l_1 = [q.get_text() for q in soup.find_all("div", {"class": "headline small normal-style text-align-inherit "})]
-l_2 = [q.get_text() for q in soup.find_all("div", {"class": "headline xx-small normal-style text-align-inherit "})]
-l_3 = [q.get_text() for q in soup.find_all("div", {"class": "headline x-small normal-style text-align-inherit "})]
-l_4 = [q.get_text() for q in soup.find_all("div", {"class": "headline small thin-style text-align-inherit "})]
-l_5 = [q.get_text() for q in soup.find_all("li", {"class": "border-bottom-hairline-top-table"})]
-put = open("headline.txt", "w", encoding='utf-8')
-put.write('\n'.join(l_1)+'\n'+'\n'.join(l_2)+'\n'+'\n'.join(l_3)+'\n'+'\n'.join(l_4)+'\n'+'\n'.join(l_5))
+put = open("soup.prettify.txt", "w", encoding='utf-8')
+put.write(soup.prettify())
 put.close()
-put = open("author.txt", "w", encoding='utf-8')
-put.write('\n'.join({q.get_text() for q in soup.find_all("span", {"class": "author vcard"})}))
-put.close()
-put = open("annotation.txt", "w", encoding='utf-8')
-put.write('\n\n'.join([q.get_text() for q in soup.find_all("div", {"class": "blurb normal normal-style "})]))
+l_5 = [
+        q.get_text()
+        for q in soup.find_all(attrs=["headline small normal-style text-align-inherit ",
+                                      "headline small thin-style text-align-inherit ",
+                                      "headline xx-small normal-style text-align-inherit ",
+                                      "headline x-small normal-style text-align-inherit ",
+                                      "caption caption-small",
+                                      "blurb normal normal-style ",
+                                      "byline",
+                                      "timestamp time"])
+      ]
+l_6 = [
+        q.get_text()
+        for q in soup.find_all("li", attrs={"class": "border-bottom-hairline-top-table"})
+      ]
+put = open("all_news.txt", "w", encoding='utf-8')
+for i in l_5:
+    put.write(i + '\n\n')
+put.write("Блок 12 тем по 4 новости в каждой\n\n")
+for i in l_6:
+    put.write(i + '\n\n')
 put.close()
